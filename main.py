b4b4719a3831c7d71f31a22d38c43fe39229511d
Your Name <mr.shikhar100@gmail.com>
2026-09-03 23:52:17 +0530

    Do changes

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+# from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 
 load_dotenv()
 
+data = PyPDFLoader()
+docs = data.load
+
+template  = ChatPromptTemplate.from_messages(
+    [
+        ("system","you are AI that summarizes the text")
+        ("human","{data}")
+    ]
+)
+
 model = ChatMistralAI(model = "mistral-small-2506")
+
+
+prompt = template.format_messages()
+
+result = model.invoke(prompt)
+
+print(result.content)
