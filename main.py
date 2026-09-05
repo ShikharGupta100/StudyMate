482bb59db23079a55bd11af5fb2f32ad7b671cf8
Your Name <mr.shikhar100@gmail.com>
2026-09-05 23:53:53 +0530

    Chunks split functionallity added

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from langchain_mistralai import ChatMistralAI
 # from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 load_dotenv()
 
 data = PyPDFLoader()
 docs = data.load
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
 
 template  = ChatPromptTemplate.from_messages(
     [
